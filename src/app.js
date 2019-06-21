41427dbaf2fbb3c5d0dcd5079dd3cfe4de911a6c
浜口健一郎 <khamaguchi@bemap.co.jp>
2019-06-21 20:14:32 +0900

    feat: use koajs

--- src/app.js
+++ src/app.js
@@ -1,10 +1,24 @@
-const http = require('http')
-const https = require('https')
+const bunyan = require('bunyan');
+const {name} = require('../package.json')
+const logger = bunyan.createLogger({
+  name: name,
+  level: bunyan.DEBUG,
+  stream: process.stdout,
+  serializers: bunyan.stdSerializers
+});
+
+const Koa = require('koa');
 const request = require('request-promise')
 
-http.createServer(async (req, res) => {
-  console.log(`${new Date().toISOString()} ${req.method} ${req.url}`)
-  const body = await request('https://www.example.com')
-  res.end(body)
+const app = new Koa();
+
+const koaBunyanLogger = require('koa-bunyan-logger');
+app.use(koaBunyanLogger(logger));
+app.use(koaBunyanLogger.requestLogger());
+
+app.use(async ctx => {
+  console.log(`${new Date().toISOString()} ${ctx.method} ${ctx.url}`)
+  ctx.body = await request('https://www.example.com')
 })
-.listen(process.env.PORT || 8080)
+
+app.listen(process.env.PORT || 8080)
